a459919078cb6dcc633e6effabbbab364139c323
Mickael Bolnet <mickael.bolnet@gmail.com>
2019-04-02 21:36:08 +0200

    relationship

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('../models/users');
+const Order = require('../models/orders');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -18,6 +19,14 @@ router.get('/profile/:id', (req, res, next) => {
   })
 });
 
+router.get('/profile/:id/orders', (req, res, next) => {
+  Order.find({owner: req.params.id})
+  .populate('owner')
+  .then(order => {
+    res.send(order);
+  })
+});
+
 router.get('/update/:id', (req, res, next) => {
   User.findOne({_id: req.params.id})
   .then(user => {
@@ -29,6 +38,16 @@ router.post('/update/:id', (req, res, next) => {
   User.updateOne({_id: req.params.id}, {$set: req.body})
   .then(user => {
     res.redirect('/profile/'+req.params.id);
+  })
+});
+
+router.post('/order/:ownerid', (req, res, next) => {
+  let new_order = req.body
+  new_order.owner = req.params.ownerid
+
+  Order.create(new_order)
+  .then(order => {
+    res.redirect('/profile/'+req.params.ownerid);
   })
 });
 
